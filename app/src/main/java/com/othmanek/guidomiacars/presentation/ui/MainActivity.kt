c65c062bf95882bf95ff9e2222f8ec95f03821da
OthmaneKa <othmane.kassabi@gmail.com>
2022-07-16 12:36:17 +0100

    Added new dependencies + Home Fragment and layouts

--- app/src/main/java/com/othmanek/guidomiacars/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/othmanek/guidomiacars/presentation/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.othmanek.guidomiacars
+package com.othmanek.guidomiacars.presentation.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
---
+++ app/src/main/java/com/othmanek/guidomiacars/presentation/ui/home/HomeFragment.kt
@@ -0,0 +1,40 @@
+package com.othmanek.guidomiacars.presentation.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.othmanek.guidomiacars.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(binding.root, savedInstanceState)
+        binding.headerContainer.apply {
+            headerTitle.text = "Tacoma 2021"
+            headerDescription.text = "Get your's now"
+
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = HomeFragment()
+    }
+}
